e235b5b20a0312dffe125939476367e41fbc1735
AshleyCahalan <a_cahalan@fanshaweonline.ca>
2021-02-17 12:51:37 -0500

    adding drop bug fix

--- script/app.js
+++ script/app.js
@@ -1,9 +1,47 @@
+// module pattern
+// iife: immediately invoked function expression
+
 (() => {
+
 	// put variables (connections to the web page / DOM) at the top
+	// constant -> something that will never change / can't be redefined
 	const puzzleSelectors = document.querySelectorAll("#buttonHolder img"),
-				dropZoneContainer = document.querySelector(".puzzle-board");
+				dropZoneContainer = document.querySelector(".puzzle-board"),
+				dragImages = document.querySelectorAll(".puzzle-image"),
+				dropZones = document.querySelectorAll(".drop-zone");
 
 	// functions go in the middle
+	function dragStart(event) {
+		console.log('started draggin');
+		// take the dragged image and move it into the drop zone
+		// move it from the left container to the drop zone (reparent it)
+		event.dataTransfer.setData("savedID", this.id)
+	}
+
+	function draggedOver(event) {
+		event.preventDefault();
+		console.log('dragged over me');
+
+		// allow an element to be dragged over and then dropped
+	}
+
+	function dropped(event) {
+		// override the browser / element's default behaviour, and do what I say!
+		event.preventDefault();
+		console.log('dropped somethin on me');
+
+		// check to see if there's already an element there
+		// if so then kill this function
+		if (this.childNodes.length > 0) { return; }
+
+
+
+		// put the dragged image into this container
+		let targetID = event.dataTransfer.getData("savedID");
+		console.log('I dragged this image:', targetID);
+
+		event.target.appendChild(document.querySelector(`#${targetID}`));
+	}
 
 	// this function runs when the bottom nav buttons are clicked
 	// it changes the bg image of the drop zone div using the style property
@@ -11,18 +49,24 @@
 		// get the custom data attribute from the clicked button
 		let currentImage = this.dataset.imageref;
 
-		// ` ` is NOT a quote. It's a JS template string
-
+		// `` is NOT a quote. it's a JavaScript template string
 		dropZoneContainer.style.backgroundImage = `url(images/backGround${currentImage}.jpg)`;
-		//Intermediate way to do it, remove let statement, 1 step instead of 2
-		dropZoneContainer.style.backgroundImage = `url(images/backGround${this.dataset.imageref}.jpg)`;
 
+
+		// this is an intermediate way to do the same something
+		// dropZoneContainer.style.backgroundImage = `url(images/backGround${this.dataset.imageref}.jpg)`;
+		// debugger;
 	}
 
 	// event handling at the bottom
 	puzzleSelectors.forEach(button => button.addEventListener("click", changeBGImage));
+	dragImages.forEach(piece => piece.addEventListener("dragstart", dragStart));
 
-	// emulate a click on the first button and run the bg image function
-	changeBGImage.call(puzzleSelector[0]);
+	dropZones.forEach(zone => {
+		zone.addEventListener("dragover", draggedOver);
+		zone.addEventListener("drop", dropped);
+	})
 
+	// emulate a click on the first bottom button and run the bg image function
+	changeBGImage.call(puzzleSelectors[0]);
 })();
